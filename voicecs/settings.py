e1c44b6e8bb5c372247a2d38b70fecee6255e181
maslina08 <44882438+maslina08@users.noreply.github.com>
2022-05-29 00:41:51 +0300

    update

--- voicecs/settings.py
+++ voicecs/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api'
+    'api',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
 ]
 
 ROOT_URLCONF = 'voicecs.urls'
@@ -114,6 +117,11 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3030',
+    'https://maslina.pythonanywhere.com'
+]
 
 
 # Static files (CSS, JavaScript, Images)
